642b7ef1ec5277a88b007d9f499aa08afd545cf5
Brecht Carlier (Involved) <brecht.carlier@involved-it.be>
2017-05-01 16:26:14 +0200

    Initial Folder Structure

--- CQRS/Controllers/HomeController.cs
+++ CQRS/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace CQRS.Controllers
+namespace Erazer.Web.Controllers
 {
     public class HomeController : Controller
     {
---
+++ Erazer.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Erazer.Domain
+{
+    public class Class1
+    {
+    }
+}
