25d2ac3d9f73c3eae217c76242f690a9a432d09e
Brecht Carlier (Involved) <brecht.carlier@involved-it.be>
2017-05-21 11:27:36 +0200

    Refactored CommentBox to a ViewComponent

---
+++ CQRS/ViewComponents/CommentBoxViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Erazer.Web.ViewComponents
+{
+    public class CommentBoxViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+           return await Task.FromResult(View(model: id));
+        }
+    }
+}
