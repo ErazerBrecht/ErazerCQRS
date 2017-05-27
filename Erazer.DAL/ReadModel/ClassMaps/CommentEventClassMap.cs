33f17bfb7203fe6e276d0bf9083dc8f0c88853e2
Brecht Carlier (Involved) <brecht.carlier@involved-it.be>
2017-05-27 19:29:59 +0200

    Added support for comment event in ReadModel

---
+++ Erazer.DAL/ReadModel/ClassMaps/CommentEventClassMap.cs
@@ -0,0 +1,16 @@
+﻿using Erazer.DAL.ReadModel.Base;
+using Erazer.Services.Queries.DTOs.Events;
+using MongoDB.Bson.Serialization;
+
+namespace Erazer.DAL.ReadModel.ClassMaps
+{
+    public class CommentEventClassMap : MongoDbClassMap<CommentEventDto>
+    {
+        public override void Map(BsonClassMap<CommentEventDto> cm)
+        {
+            cm.SetDiscriminator("comment");
+
+            cm.MapField("_comment").SetElementName("comment");
+        }
+    }
+}
---
+++ Erazer.Services/Queries/DTOs/Events/CommentEventDto.cs
@@ -0,0 +1,14 @@
+﻿using Erazer.Domain.Constants.Enums;
+
+namespace Erazer.Services.Queries.DTOs.Events
+{
+    public class CommentEventDto : BaseEventDto
+    {
+        public override EventType Type => EventType.Comment;
+
+        public override string From => null;
+        public override string To => _comment;
+
+        private string _comment;
+    }
+}
