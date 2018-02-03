e9896da0dc0e270382cf9205585cf2332b2cdf45
ErazerBrecht <erazerbrecht@gmail.com>
2018-02-03 23:24:05 +0100

    Fixed missing ClassMap (TicketCreated)

---
+++ Erazer.DAL/ReadModel/ClassMaps/TicketCreatedEventClassMap.cs
@@ -0,0 +1,14 @@
+﻿using Erazer.DAL.ReadModel.Base;
+using Erazer.Domain.Infrastructure.DTOs.Events;
+using MongoDB.Bson.Serialization;
+
+namespace Erazer.DAL.ReadModel.ClassMaps
+{
+    public class TicketCreatedEventClassMap : MongoDbClassMap<CreatedEventDto>
+    {
+        public override void Map(BsonClassMap<CreatedEventDto> cm)
+        {
+            cm.SetDiscriminator("created");
+        }
+    }
+}
