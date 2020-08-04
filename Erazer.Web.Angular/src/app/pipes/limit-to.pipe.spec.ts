122a0f67fb9a924e82772f794205438fdfa6f168
ErazerBrecht <brecht.carlier@student.ap.be>
2020-08-04 22:28:05 +0200

    Removed Angular tests + deps

--- Erazer.Web.Angular/src/app/pipes/limit-to.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LimitToPipe } from './limit-to.pipe';
-
-describe('LimitToPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LimitToPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
